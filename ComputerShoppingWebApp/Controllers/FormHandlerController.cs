48e488040a03dc86f97d63b8fc6b3dade57881fd
Jahanzaib <work.officialjz@gmail.com>
2022-10-30 14:33:25 +0500

    Successfully created input form and form handler.

---
+++ ComputerShoppingWebApp/Controllers/FormHandlerController.cs
@@ -0,0 +1,42 @@
+﻿using ComputerShoppingWebApp.Models;
+using ComputerShoppingWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+
+namespace ComputerShoppingWebApp.Controllers
+{
+    [Route("[controller]")]
+    public class FormHandlerController : Controller
+    {
+        JsonFileComputerService computerService;
+        public FormHandlerController(JsonFileComputerService computerService)
+        {
+            this.computerService = computerService;
+        }
+        [HttpGet]
+        public string Get(int id, string brand, string model, string category, string price, string url)
+        {
+            Computer obj = new Computer();
+            obj.computerId = id;
+            obj.computerBrand = brand;
+            obj.computerModel = model;
+            obj.computerCategory = category;
+            obj.computerPrice = price;
+            obj.computerImage = url;
+
+            IEnumerable<Computer> computersData = computerService.getComputersRecord();
+            List<Computer> computersList = computersData.ToList();
+            computersList.Add(obj);
+
+            return JsonSerializer.Serialize<List<Computer>>(computersList);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
